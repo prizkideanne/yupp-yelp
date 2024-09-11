f27a027a9cda87ded77d03ab83355d48297c7193
prizkideanne <prizkideanne@gmail.com>
2024-09-11 16:18:50 +0700

    change baseurl

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3`;
-// const baseURL = `https://api.yelp.com/v3/businesses`;
+// const baseURL = `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3`;
+const baseURL = `https://api.yelp.com/v3/businesses`;
 
 const api = axios.create({
   baseURL,
